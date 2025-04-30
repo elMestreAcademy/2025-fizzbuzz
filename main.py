053d14763d526c7f796fa150196757a269807125
elMestre <carmesce@gmail.com>
2025-04-30 12:38:19 +0200

    pycodestyle

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ contador = 100
 
 while (contador >= 1):
     if contador % 3 == 0 and contador % 5 == 0:
-        print ("fizzbuzz")
-    elif contador % 3 == 0: 
+        print("fizzbuzz")
+    elif contador % 3 == 0:
         print("fizz")
     elif contador % 5 == 0:
         print("buzz")
